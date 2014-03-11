d6043f5f73fddaa1812277e1563da2f803160366
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-03-11 12:43:31 +0100

    Testing py2app build.

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 # -*- coding: UTF-8 -*-
-# -----------------------------------------------------------------------------
-#     Xierpa 3 Application
-#     (c) 2014  buro@petr.com, www.petr.com, www.xierpa.com
 #
-#     X I E R P A  3
-#     No distribution without permission.
+#    X I E R P A   3
+#    OS X Application (c) 2014 buro@petr.com, www.petr.com, www.xierpa.com
+#    Authors: Petr van Blokland, Michiel Kauw–A–Tjoe
 #
-# -----------------------------------------------------------------------------
+#    No distribution without permission.
 #
-# sudo python setup.py py2app
+#    python setup.py py2app
 #
 
 from distutils.core import setup
@@ -18,14 +16,19 @@ from plistlib import readPlist, writePlist
 
 appName = "Xierpa3App"
 
-plist = dict(CFBundleIdentifier="com.petr.xierpa3app", LSMinimumSystemVersion="10.6.0",
-             CFBundleShortVersionString="1.0.0", CFBundleVersion="1.0.0", CFBundleIconFile="icon.icns",)
+plist = dict(CFBundleIdentifier="com.petr.xierpa3app",
+             LSMinimumSystemVersion="10.6.0",
+             CFBundleShortVersionString="1.0.0",
+             CFBundleVersion="1.0.0",
+             CFBundleIconFile="icon.icns",)
 
 dataFiles = ['Resources/English.lproj', ]
 setup(data_files=dataFiles, app=[dict(script="Xierpa3App.py", plist=plist)],)
 
 # correct the CFBundleIconFile
+'''
 infoPlistPath = os.path.join(os.path.dirname(__file__), "dist", "%s.app" % appName, "Contents", "Info.plist")
 plist = readPlist(infoPlistPath)
 plist["CFBundleIconFile"] = "icon.icns"
 writePlist(plist, infoPlistPath)
+'''
