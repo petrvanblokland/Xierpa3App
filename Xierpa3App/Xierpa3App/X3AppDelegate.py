cd81c98e207d75e3935e5e13ca3f4398dd85766d
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-05-27 16:36:26 +0200

    Setting up Xcode project.

---
+++ Xierpa3App/Xierpa3App/X3AppDelegate.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+#
+#  Xierpa3AppAppDelegate.py
+#  Xierpa3App
+#
+#  Created by Michiel on 27/05/14.
+#  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+#
+
+from Foundation import *
+from AppKit import *
+
+class X3AppDelegate(NSObject):
+    def applicationDidFinishLaunching_(self, sender):
+        NSLog("Application did finish launching.")
