2eecad754237f074c3ecc528083f0b43d2b684f8
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-05-28 11:28:36 +0200

    Moving existing files into Xcode project.

--- Xierpa3App/main.py
+++ Xierpa3App/main.py
@@ -1,21 +1,5 @@
-#-*- coding: utf-8 -*-
-#
-#  main.py
-#  Xierpa3App
-#
-#  Created by Michiel on 27/05/14.
-#  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
-#
-
-#import modules required by application
-import objc
-import Foundation
-import AppKit
-
 from PyObjCTools import AppHelper
 
-# import modules containing classes required to start application and load MainMenu.nib
-import X3AppDelegate
+import xierpa3app.XierpaAppDelegate #@UnusedImport
 
-# pass control to AppKit
 AppHelper.runEventLoop()
