d54fa466b695b15cdb19f4d32372dec9e264f22b
petrvanblokland <buro@petr.com>
2014-06-26 23:14:08 +0200

    Disable PHP button for now.

--- Xierpa3App/xierpa3app.py
+++ Xierpa3App/xierpa3app.py
@@ -15,7 +15,8 @@ from constants import AppC
 from vanilla import RadioGroup, Window, Button, CheckBox, EditText, TextEditor
 from xierpa3.sites.doingbydesign.doingbydesign import DoingByDesign
 from xierpa3.builders.cssbuilder import CssBuilder
-from xierpa3.builders.phpbuilder import PhpBuilder 
+#from xierpa3.builders.htmlbuilder import HtmlBuilder 
+from xierpa3.builders.phpbuilder import PhpBuilder
 from xierpa3.adapters import PhpAdapter
 #from xierpa3.adapters.kirby.kirbyadapter import KirbyAdapter
 from xierpa3.constants.constants import C
@@ -62,7 +63,8 @@ print page.name
         self.w.openCss = Button((10, y+45, 150, 20), 'Open CSS', callback=self.openCssCallback, sizeStyle='small')
         self.w.openSass = Button((10, y+70, 150, 20), 'Open SASS', callback=self.openSassCallback, sizeStyle='small')
         self.w.openDocumentation = Button((10, y+95, 150, 20), 'Documentation', callback=self.openDocumentationCallback, sizeStyle='small')
-        self.w.saveAsKirby = Button((10, y+120, 150, 20), 'Save as Kirby', callback=self.saveAsKirbyCallback, sizeStyle='small')
+        self.w.openAsPhp = Button((10, y+120, 150, 20), 'Open as PHP', callback=self.openAsPhpCallback, sizeStyle='small')
+        self.w.openAsPhp.enable(False) # For now.
         #self.w.makeSite = Button((10, y+95, 150, 20), 'Make site', callback=self.makeSiteCallback, sizeStyle='small')
         self.w.forceCss = CheckBox((180, 10, 150, 20), 'Force make CSS', sizeStyle='small')
         self.w.doIndent = CheckBox((180, 30, 150, 20), 'Build indents', sizeStyle='small', value=True)
@@ -89,11 +91,13 @@ print page.name
         pass
 
     def openSiteCallback(self, sender):
-        url = self.URL
+        self.openSiteInBrowser(self.URL)
+
+    def openSiteInBrowser(self, url):
         if self.w.forceCss.get():
             url += '/' + C.PARAM_FORCE
         webbrowser.open(url)
-
+    
     def openCssCallback(self, sender):
         url = self.URL
         if self.w.forceCss.get():
@@ -108,25 +112,27 @@ print page.name
         url = self.URL
         webbrowser.open(url + '/' + C.PARAM_DOCUMENTATION + '/' + C.PARAM_FORCE)
 
-    def saveAsKirbyCallback(self, sender):
-        # Save site as Kirby template in MAMP area.
+    def openAsPhpCallback(self, sender):
+        u"""Save site as PHP template in MAMP area and then open it in the browser.
+        This function assumes that a PHP server like MAMP is running. Otherwise the
+        page will not open in the browser."""
+        # TODO: Root of the MAMP file. Should become a config variable.
         ROOT_MAMP = '/Applications/MAMP/htdocs/'
         # Get the current selected site instance.
         site = self.getSite()
         saveAdapter = site.adapter # Save the current adapter for this site.
         site.adapter = PhpAdapter() # Create the site running with this adapter.
+        rootPath = ROOT_MAMP + site.__class__.__name__.lower() + '/' # TODO: Ask for save folder instead
         # Create the main blog builder, which will split into building the
         # CSS and PHP/HTML files, using the Kirby PHP snippets as content.
         cssBuilder = CssBuilder()
         site.build(cssBuilder) # Build from entire site theme, not just from template. Result is stream in builder.
-        rootPath = ROOT_MAMP + site.__class__.__name__.lower() + '/assets/' # TODO: Ask for save folder instead
         cssBuilder.save(site, root=rootPath)
         # Build the PHP/HTML template.
         phpBuilder = PhpBuilder()
         # Make the PHP source directly save to MAMP, so it is served by local server.
         # Build the CSS and and PHP/HTML files in the MAMP directory.
         site.build(phpBuilder) # Build from entire site theme, not just from template. Result is stream in builder.
-        rootPath = ROOT_MAMP + site.__class__.__name__.lower() + '/site/templates/' # TODO: Ask for save folder instead
         phpBuilder.save(site, root=rootPath)
         # Restore the original adapter.
         site.adapter = saveAdapter
