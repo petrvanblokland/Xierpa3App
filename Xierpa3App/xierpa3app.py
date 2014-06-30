c7164f6e78a912664d19bf665c5195f3a1228608
petrvanblokland <buro@petr.com>
2014-06-30 16:48:19 +0200

    Cleaning window title stuff

--- Xierpa3App/xierpa3app.py
+++ Xierpa3App/xierpa3app.py
@@ -10,14 +10,18 @@
 #
 #    xierpa3app.py
 #
+import os
 import webbrowser
+from AppKit import NSInformationalAlertStyle #@UnresolvedImport
 from constants import AppC
-from vanilla import RadioGroup, Window, Button, CheckBox, EditText, TextEditor
+from vanilla import RadioGroup, Window, Button, CheckBox, EditText, TextEditor, TextBox
+from vanilla.dialogs import message
 from xierpa3.sites.doingbydesign.doingbydesign import DoingByDesign
+from xierpa3.builders.sassbuilder import SassBuilder
 from xierpa3.builders.cssbuilder import CssBuilder
-#from xierpa3.builders.htmlbuilder import HtmlBuilder 
-from xierpa3.builders.phpbuilder import PhpBuilder
-from xierpa3.adapters import PhpAdapter
+from xierpa3.builders.htmlbuilder import HtmlBuilder 
+from xierpa3.builders import PhpBuilder
+#from xierpa3.adapters import PhpAdapter
 #from xierpa3.adapters.kirby.kirbyadapter import KirbyAdapter
 from xierpa3.constants.constants import C
 from xierpa3.sites.examples import HelloWorld, HelloWorldLayout, HelloWorldBluePrint, \
@@ -52,29 +56,48 @@ print page.name
         u"""
         Initialize the window and open it.
         """
-        self.w = Window((AppC.WINDOW_WIDTH, AppC.WINDOW_HEIGHT), "Xierpa 3",
+        self.w = view = Window((AppC.WINDOW_WIDTH, AppC.WINDOW_HEIGHT), "Xierpa 3",
             closable=True, minSize=(200, 200), maxSize=(1600, 1000))
         siteLabels = self.getSiteLabels()
         y = len(siteLabels)*20
-        self.w.optionalSites = RadioGroup((10, 10, 150, y), siteLabels,
+        bo = 25 # Button offset
+        view.optionalSites = RadioGroup((10, 10, 150, y), siteLabels,
             callback=self.selectSiteCallback, sizeStyle='small')
         self.w.optionalSites.set(0)
-        self.w.openSite = Button((10, y+20, 150, 20), 'Open site', callback=self.openSiteCallback, sizeStyle='small')
-        self.w.openCss = Button((10, y+45, 150, 20), 'Open CSS', callback=self.openCssCallback, sizeStyle='small')
-        self.w.openSass = Button((10, y+70, 150, 20), 'Open SASS', callback=self.openSassCallback, sizeStyle='small')
-        self.w.openDocumentation = Button((10, y+95, 150, 20), 'Documentation', callback=self.openDocumentationCallback, sizeStyle='small')
-        self.w.openAsPhp = Button((10, y+120, 150, 20), 'Open as PHP', callback=self.openAsPhpCallback, sizeStyle='small')
-        self.w.openAsPhp.enable(False) # For now.
-        #self.w.makeSite = Button((10, y+95, 150, 20), 'Make site', callback=self.makeSiteCallback, sizeStyle='small')
-        self.w.forceCss = CheckBox((180, 10, 150, 20), 'Force make CSS', sizeStyle='small')
-        self.w.doIndent = CheckBox((180, 30, 150, 20), 'Build indents', sizeStyle='small', value=True)
-        self.w.console = EditText((10, -200, -10, -10), sizeStyle='small')
-        self.w.script = TextEditor((300, 10, -10, -240))
-        self.w.runScript = Button((300, -230, 150, -210), 'Run script', 
+        y = y + 20
+        view.openSite = Button((10, y, 150, 20), 'Open site', callback=self.openSiteCallback, sizeStyle='small')
+        y += bo
+        self.w.saveSite = Button((10, y, 150, 20), 'Save HTML+CSS', callback=self.saveSiteCallback, sizeStyle='small')
+        y += bo
+        view.openCss = Button((10, y, 150, 20), 'Open CSS', callback=self.openCssCallback, sizeStyle='small')
+        y += bo
+        view.openSass = Button((10, y, 150, 20), 'Open SASS', callback=self.openSassCallback, sizeStyle='small')
+        y += bo
+        view.openDocumentation = Button((10, y, 150, 20), 'Documentation', callback=self.openDocumentationCallback, sizeStyle='small')
+        y += bo
+        view.openAsPhp = Button((10, y, 150, 20), 'Open as PHP', callback=self.openAsPhpCallback, sizeStyle='small')
+        #view.makeSite = Button((10, y+95, 150, 20), 'Make site', callback=self.makeSiteCallback, sizeStyle='small')
+        view.forceCss = CheckBox((180, 10, 150, 20), 'Force make CSS', sizeStyle='small')
+        view.doIndent = CheckBox((180, 30, 150, 20), 'Build indents', sizeStyle='small', value=True)
+        view.console = EditText((10, -200, -10, -10), sizeStyle='small')
+        # Path defaults
+        y = 20
+        view.mampRootLabel = TextBox((300, y, 100, 20), 'MAMP folder', sizeStyle='small')
+        view.mampRoot = EditText((400, y, -10, 20), C.PATH_MAMP, sizeStyle='small')
+        y += bo
+        view.exampleRootLabel = TextBox((300, y, 100, 20), 'Root folder', sizeStyle='small')
+        view.exampleRoot = EditText((400, y, -10, 20), C.PATH_EXAMPLES, sizeStyle='small') 
+        # Scripting
+        y += bo
+        view.script = TextEditor((300, y, -10, -240))
+        view.runScript = Button((500, -230, 150, -210), 'Run script', 
             callback=self.runScriptCallback, sizeStyle='small')
-        self.w.script.set(self.EXAMPLE_SCRIPT)
-        self.w.open()
+        view.script.set(self.EXAMPLE_SCRIPT)
+        view.open()
 
+    def getView(self):
+        return self.w
+        
     def getSiteLabels(self):
         siteLabels = []
         for siteLabel, _ in self.SITE_LABELS:
@@ -82,7 +105,8 @@ print page.name
         return siteLabels
 
     def runScriptCallback(self, sender):
-        src = self.BASESCRIPT + self.w.script.get()
+        view = self.getView()
+        src = self.BASESCRIPT + view.script.get()
         cc = compile(src, 'abc', mode='exec')
         eval(cc, {'currentSite': self.getSite()})
         #, self.getSite().__dict__)
@@ -93,14 +117,33 @@ print page.name
     def openSiteCallback(self, sender):
         self.openSiteInBrowser(self.URL)
 
+    def updateBuilderRootPaths(self):
+        view = self.getView()
+        rootPath = view.exampleRoot.get()
+        HtmlBuilder.ROOT_PATH = rootPath
+        SassBuilder.ROOT_PATH = rootPath
+        CssBuilder.ROOT_PATH = rootPath
+        
+    def saveSiteCallback(self, sender):
+        self.updateBuilderRootPaths()
+        site = self.getSite()
+        site.make()
+        path = self.getExampleRootPath(site)
+        if path is not None:
+            webbrowser.open('file:' + path)
+        
     def openSiteInBrowser(self, url):
-        if self.w.forceCss.get():
+        self.updateBuilderRootPaths()
+        view = self.getView()
+        if view.forceCss.get():
             url += '/' + C.PARAM_FORCE
         webbrowser.open(url)
     
     def openCssCallback(self, sender):
+        view = self.getView()
+        self.updateBuilderRootPaths()
         url = self.URL
-        if self.w.forceCss.get():
+        if view.forceCss.get():
             url += '/' + C.PARAM_FORCE
         webbrowser.open(url + '/css/style.css')
 
@@ -109,36 +152,60 @@ print page.name
         #os.open(url + '/css/style.scss')
 
     def openDocumentationCallback(self, sender):
+        self.updateRootPaths()
         url = self.URL
         webbrowser.open(url + '/' + C.PARAM_DOCUMENTATION + '/' + C.PARAM_FORCE)
 
+    def getMampRootPath(self, site):
+        view = self.getView()
+        root = os.path.expanduser(view.mampRoot.get()) # File root of server.
+        if not root.endswith('/'):
+            root += '/'
+        if not os.path.exists(root):
+            message(messageText='Error in MAMP path.', informativeText='The MAMP folder "%s" does not exist.' % root, 
+                alertStyle=NSInformationalAlertStyle, parentWindow=view)
+            return None
+        return root + site.__class__.__name__.lower() + '/' 
+    
+    def getExampleRootPath(self, site):
+        view = self.getView()
+        root = os.path.expanduser(view.exampleRoot.get()) # File root of server.
+        if not root.endswith('/'):
+            root += '/'
+        if not os.path.exists(root):
+            message(messageText='Error in Examples path.', informativeText='The Examples folder "%s" does not exist.' % root, 
+                alertStyle=NSInformationalAlertStyle, parentWindow=view)
+            return None
+        return root + site.__class__.__name__.lower() + '/' 
+        
     def openAsPhpCallback(self, sender):
         u"""Save site as PHP template in MAMP area and then open it in the browser.
         This function assumes that a PHP server like MAMP is running. Otherwise the
         page will not open in the browser."""
-        # TODO: Root of the MAMP file. Should become a config variable.
-        ROOT_MAMP = '/Applications/MAMP/htdocs/'
         # Get the current selected site instance.
         site = self.getSite()
-        saveAdapter = site.adapter # Save the current adapter for this site.
-        site.adapter = PhpAdapter() # Create the site running with this adapter.
-        rootPath = ROOT_MAMP + site.__class__.__name__.lower() + '/' # TODO: Ask for save folder instead
-        # Create the main blog builder, which will split into building the
-        # CSS and PHP/HTML files, using the Kirby PHP snippets as content.
-        cssBuilder = CssBuilder()
-        site.build(cssBuilder) # Build from entire site theme, not just from template. Result is stream in builder.
-        cssBuilder.save(site, root=rootPath)
-        # Build the PHP/HTML template.
-        phpBuilder = PhpBuilder()
-        # Make the PHP source directly save to MAMP, so it is served by local server.
+        # Save the current adapter for this site in order to restore it in the end.
+        # The site instance is create on startup, and we don't want to destroy
+        # the original default adapter that is already there.
+        saveAdapter = site.adapter 
+        #site.adapter = PhpAdapter() # Create the site running with this adapter.
+        rootPath = self.getMampRootPath(site)
         # Build the CSS and and PHP/HTML files in the MAMP directory.
-        site.build(phpBuilder) # Build from entire site theme, not just from template. Result is stream in builder.
-        phpBuilder.save(site, root=rootPath)
+        builder = CssBuilder()
+        site.build(builder) # Build from entire site theme, not just from template. Result is stream in builder.
+        builder.save(site, root=rootPath)
+        # Create the PhpBuilder instance that can build/modify the PHP file structure.
+        builder = PhpBuilder()
+        # Render the website towards PHP export.
+        site.build(builder) # Build from entire site theme, not just from template. Result is stream in builder.
+        # Copy the PHP frame work and save PHP/HTML files,
+        builder.save(site, root=rootPath)
         # Restore the original adapter.
         site.adapter = saveAdapter
            
     def getSite(self):
-        _, site = self.SITE_LABELS[self.w.optionalSites.get()]
+        view = self.getView()
+        _, site = self.SITE_LABELS[view.optionalSites.get()]
         return site
 
     def makeSiteCallback(self, sender):
@@ -148,9 +215,11 @@ print page.name
         self.addConsole(`httprequest` + ' ' + `site.e.form`)
 
     def addConsole(self, s):
-        self.w.console.set(self.w.console.get() + '\n' + s)
+        view = self.getView()
+        view.console.set(view.console.get() + '\n' + s)
 
     def getDoIndent(self):
         u"""Answer true if building output code with indent."""
-        return self.w.doIndent.get()
+        view = self.getView()
+        return view.doIndent.get()
 
